10d1f0b7903a5386227bd41d1d53f6d4610e4721
kivy0000 <kivy0000@126.com>
2024-02-02 10:18:17 +0800

    only Login.vue

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+//引入elementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//使用ElementPlus .use(ElementPlus)
+
+//国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+createApp(App).use(store).use(router).use(ElementPlus,{locale: zhCn,}).mount('#app')
